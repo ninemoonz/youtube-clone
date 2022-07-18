a1701c96d4707019729a8cbf78e119d8b0c718d2
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-18 19:32:09 +0900

    GET Request and Response

--- src/server.js
+++ src/server.js
@@ -2,8 +2,22 @@ import express from "express";
 
 const PORT = 4000;
 
+// Creating a server
 const app = express();
 
-const handleListening = () => console.log(`Server is listening on port http://localhost:${PORT}`);
+// This is the place where we write configuration of the server
+const handleMain = (req, res) => {
+    return res.send("Wake up, Neo...");
+};
 
+const handleLogin = (req, res) => {
+    return res.send({redPill: "You stay here."});
+}
+
+app.get("/", handleMain);
+app.get("/login", handleLogin);
+
+
+// Making the server to listen
+const handleListening = () => console.log(`Server is listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
