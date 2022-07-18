96ab3b1a6220eff1b76268e5a21265c944ee79d2
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-19 02:20:55 +0900

    External middleware - Morgan

--- src/server.js
+++ src/server.js
@@ -1,22 +1,25 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 // Creating a server
 const app = express();
+const logger = morgan("dev");
 
 //middleware
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
-
 // This is the place where we write configuration of the server
 const handleMain = (req, res) => {
-    return res.send("I'm at the end");
+    return res.send("Main Page");
 };
 
-app.get("/", logger, handleMain);
+const handleLogin = (req, res) => {
+    return res.send("Login success");
+}
+
+app.use(logger);
+app.get("/", handleMain);
+app.get("/login", handleLogin);
 
 // Making the server to listen
 const handleListening = () => console.log(`Server is listening on port http://localhost:${PORT}`);
