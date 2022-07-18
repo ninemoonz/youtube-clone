b7b6983a4041469ec0cda13e3a4da28c0a93cadf
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-19 08:41:05 +0900

    Cleaning the code
    - Separating the three routers into each js files in a router folder
    
    - export default and import the router variables

--- src/server.js
+++ src/server.js
@@ -1,28 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/globalRouter";
+import videoRouter from "./routers/globalRouter";
+
 const PORT = 4000;
 
 // Creating a server
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
-//routers
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-const handleListVideo = (req, res) => res.send("List of Videos");
-videoRouter.get("", handleListVideo);
-videoRouter.get("/watch", handleWatchVideo);
-
 
 app.use("/", globalRouter); 
 app.use("/videos", videoRouter);
