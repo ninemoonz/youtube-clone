45721774515d697cffa996e389368f68700ffc09
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-19 04:10:37 +0900

    Routers

--- src/server.js
+++ src/server.js
@@ -6,20 +6,29 @@ const PORT = 4000;
 // Creating a server
 const app = express();
 const logger = morgan("dev");
-
-//middleware
-// This is the place where we write configuration of the server
-const handleMain = (req, res) => {
-    return res.send("Main Page");
-};
-
-const handleLogin = (req, res) => {
-    return res.send("Login success");
-}
-
 app.use(logger);
-app.get("/", handleMain);
-app.get("/login", handleLogin);
+
+//routers
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+const handleListVideo = (req, res) => res.send("List of Videos");
+videoRouter.get("", handleListVideo);
+videoRouter.get("/watch", handleWatchVideo);
+
+
+app.use("/", globalRouter); 
+app.use("/videos", videoRouter);
+app.use("/user", userRouter);
+
+
 
 // Making the server to listen
 const handleListening = () => console.log(`Server is listening on port http://localhost:${PORT}`);
