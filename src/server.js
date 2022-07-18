b41218a50002a8ad867bca807fb825fda4970040
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-19 00:26:41 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -5,18 +5,18 @@ const PORT = 4000;
 // Creating a server
 const app = express();
 
-// This is the place where we write configuration of the server
-const handleMain = (req, res) => {
-    return res.send("Wake up, Neo...");
+//middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
 };
 
-const handleLogin = (req, res) => {
-    return res.send({redPill: "You stay here."});
-}
+// This is the place where we write configuration of the server
+const handleMain = (req, res) => {
+    return res.send("I'm at the end");
+};
 
-app.get("/", handleMain);
-app.get("/login", handleLogin);
-
+app.get("/", logger, handleMain);
 
 // Making the server to listen
 const handleListening = () => console.log(`Server is listening on port http://localhost:${PORT}`);
