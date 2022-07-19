9d211493c549f9a826abebc94390b021cdc381b5
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-19 13:05:59 +0900

    URL parameters, regex

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,16 @@
 export const trending = (req, res) => res.send("Main page video lists");
 
-export const see = (req, res) => res.send("See Video");
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => {
+    return res.send(`Watch the vid #${req.params.id}`);
+};
+export const edit = (req, res) => {
+    
+    return res.send("Edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.upload("upload");
 
-export const deleteVideo = (req, res) => res.upload("Delete video");
+export const deleteVideo = (req, res) => {
+   
+   return res.send("Delete video");
+};
