34bf748f7efc6fadfb54865d209c27ed368a8629
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-19 10:51:55 +0900

    Planning routes (URLs)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,8 @@
 export const trending = (req, res) => res.send("Main page video lists");
-export const watch = (req, res) => res.send("Watch");
+
+export const see = (req, res) => res.send("See Video");
 export const edit = (req, res) => res.send("Edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.upload("upload");
+
+export const deleteVideo = (req, res) => res.upload("Delete video");
--- src/server.js
+++ src/server.js
@@ -17,7 +17,6 @@ app.use("/video", videoRouter);
 app.use("/user", userRouter);
 
 
-
 // Making the server to listen
 const handleListening = () => console.log(`Server is listening on port http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
