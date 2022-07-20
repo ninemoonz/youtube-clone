216933732392bf06edf51c1e2ffcd35da55c591e
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-20 15:43:33 +0900

    conditionals: if else

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+    username: "Koodal",
+    loggedIn: false,
+}
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
 
 export const see = (req, res) => res.render("watch", { pageTitle: "VideoTitle"});
 
