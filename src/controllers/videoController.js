db3ac9639c1879b5e66c1d92fca139fb1922d627
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-21 22:56:28 +0900

    upload form
    - More practice in POST method

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -41,5 +41,24 @@ export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
     videos[id - 1].title = title;
+    console.log(title);
     return res.redirect(`/videos/${id}`);
 };
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+    // Here we add a video in the videos array.
+    const { title } = req.body;
+    const newVideo = {
+        title,
+        rating: 0,
+        comments: 0,
+        createdAt: "Just Now",
+        views: 0,
+        id: videos.length + 1,
+    }
+    videos.push(newVideo);
+    return res.redirect("/");
+
+}
