d9388cdf363ce56b89e63ad34fd81659e1d9a289
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-23 00:21:20 +0900

    hexadecimal regex, watch page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -6,9 +6,10 @@ export const home = async(req, res) => {
     return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async(req, res) => {
     const { id } = req.params;
-    return res.render("watch", { pageTitle: `You're Watching ${video.title}`, });
+    const video = await Video.findById(id);
+    return res.render("watch", { pageTitle: `You're Watching: ${video.title}`, video });
 };
 export const getEdit = (req, res) =>  {
     const { id } = req.params;
