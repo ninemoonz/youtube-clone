deec4a97dd1a2119c55a6ae15734f1d1eb95e78e
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-30 21:25:13 +0900

    File Uploads
    - Multer package
    -

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -158,9 +158,10 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
     const {
         session: {
-            user: {_id},
+            user: {_id, avatarUrl},
         },
-        body: {name, email, username, location},
+        body: {name, email, username, location, },
+        file, 
     } = req;
     
     /*
@@ -184,6 +185,7 @@ export const postEdit = async (req, res) => {
     };
 
     const updatedUser = await User.findByIdAndUpdate(_id, {
+        avatarUrl: file ? file.path : avatarUrl,
         name,
         email,
         username,
