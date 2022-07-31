4e202c430a054b03325dfd3748990edfc2d7823d
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-31 21:11:03 +0900

    video owner
    - making relationship between user model and video model
    - Showing who uploaded the video
    - Showing all the videos uploaded by a user. (long way)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import bcrypt from "bcrypt";
 import fetch from "node-fetch";
 
@@ -192,12 +193,8 @@ export const postEdit = async (req, res) => {
         location,
     },
     {new: true});
-
-    
-
     req.session.user = updatedUser;
     res.redirect("/users/edit");
-
 };
 
 export const getChangePassword = (req, res) => {
@@ -236,10 +233,17 @@ export const postChangePassword = async (req, res) => {
 export const see = async (req, res) => {
     const {id} = req.params;
     const user = await User.findById(id);
+    
     if(!user){
         return res.status(404).render("404", {pageTitle: "User not found"});
     }
+
+    const videos = await Video.find({owner: user._id});
+    console.log(videos);
+
     return res.render("users/profile", {
-        pageTitle: user.name, 
-        user});
+        pageTitle: user.username, 
+        user,
+        videos,
+    });
 } 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 export const home = async(req, res) => {
     const videos = await Video.find({}).sort({createdAt:"desc"});
@@ -6,8 +7,10 @@ export const home = async(req, res) => {
 };
 
 export const watch = async(req, res) => {
-    const { id } = req.params;
-    const video = await Video.findById(id);
+    const { id } = req.params; 
+    const video = await Video.findById(id).populate("owner");
+    console.log(video);
+
     if(!video){        
         return res.status(404).render("404", {pageTitle: "404 Error, Video not found"});
     }
@@ -43,6 +46,7 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+    const {user: {_id}} = req.session;
     const {path:fileUrl} = req.file;
     const { title, description, hashtags } = req.body;
     try{
@@ -50,6 +54,7 @@ export const postUpload = async (req, res) => {
             title, //abbreviation of title: title, 
             description,
             fileUrl,
+            owner: _id,
             hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
