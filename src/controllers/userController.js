49337d1e9063229a37fd31ae15d951b1b7fa952d
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-07-31 18:45:54 +0900

    user profile section

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -233,4 +233,13 @@ export const postChangePassword = async (req, res) => {
     // send notification
     return res.redirect("/users/logout");
 };
-export const see = (req, res) => res.send("Watch video");
+export const see = async (req, res) => {
+    const {id} = req.params;
+    const user = await User.findById(id);
+    if(!user){
+        return res.status(404).render("404", {pageTitle: "User not found"});
+    }
+    return res.render("users/profile", {
+        pageTitle: user.name, 
+        user});
+} 
