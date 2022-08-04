0d4206eb46c22a5efbd04f352cacd60d351f081c
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-08-04 18:35:30 +0900

    Video Player setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,14 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
    plugins: [new MiniCssExtractPlugin({
     filename: "css/styles.css",
    })], 
-   entry: "./src/client/js/main.js",
+   entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
    watch: true,
    mode: "development",
    output: {
-     filename: "js/main.js",
+     filename: "js/[name].js",
      path: path.resolve(__dirname, "assets"),
      clean: true,
    },
