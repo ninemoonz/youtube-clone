31074633eb7dfb760e4e4d888190b0c27872a696
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-08-04 19:02:45 +0900

    Video play and pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,26 @@
-console.log("Hello! Video Player");
+// DOM
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+};
+const handlePause = () => (playBtn.innerHTML = "Pause");
+const handlePaly = () => (playBtn.innerHTML = "Play");
+
+const handleMute = (e) => {
+};
+
+
+// Event Listener
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
