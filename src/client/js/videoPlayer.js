4afc8abcaa4d6b7c974e9758a9e815266f0e2093
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-08-06 22:39:30 +0900

    timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,6 +5,7 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -44,15 +45,22 @@ const handleVolumeChange = (event) => {
 
 const handleLoadedMetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
-    
     currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
 };
+
+const handleTimelineChange= (event) => {
+    const {target: {value}} = event;
+    video.currentTime = value;
+}
 // Event Listener
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
