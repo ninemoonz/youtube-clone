5bdf929e75cf5829b6d15fccfd43122eaea3c36e
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-08-13 19:54:20 +0900

    flash messages with mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -49,6 +49,7 @@ export const postEdit = async(req, res) => {
         description,
         hashtags: Video.formatHashtags(hashtags),
     });
+    req.flash("success", "Change saved");
     return res.redirect(`/videos/${id}`);
 };
 
